c01d5eebc998a4343a381834c898d0a4a0e09d6e
Dani Sanchez <danysq87@gmail.com>
2021-10-24 12:39:52 +0200

    Added FormThree

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import "./App.css";
 // import { Button, Form, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FormTwo from "./components/FormTwo/FormTwo";
+import FormOne from "./components/FormOne/FormOne";
+import FormThree from "./components/FormThree/FormThree";
 
 function App() {
-  return (
-    <div className="App">
-      <FormTwo />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/components/FormTwo/FormTwo.js
+++ src/components/FormTwo/FormTwo.js
@@ -3,36 +3,57 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const FormTwo = () => {
   return (
     <form>
-      <div className="App formTwo">
-        <h1>FORM</h1>
-        <div className="mb-2 col-sm-10">
-          <label htmlFor="username" className="form-label">
-            UserName
-          </label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            className="form-control"
-            placeholder="UserName"
-            autoComplete="off"
-            required
-          />
-        </div>
-        <label for="inputPassword2" class="col-sm-2 col-form-label">
-          Password
+      <h1>Create your Username</h1>
+      <div className="mb-2 col-sm-10">
+        <label htmlFor="username" className="form-label">
+          Username
         </label>
-        <div class="col-sm-10">
-          <input type="password" class="form-control" id="inputPassword2" />
-        </div>
-        <label for="inputPassword3" class="col-sm-2 col-form-label">
-          Password
-        </label>
-        <div class="col-sm-10">
-          <input type="password" class="form-control" id="inputPassword3" />
-        </div>
-        <input type="submit" value="Submit" className="btn btn-primary" />
+        <input
+          type="text"
+          name="username"
+          id="username"
+          className="form-control"
+          placeholder="UserName"
+          autoComplete="off"
+          required
+        />
       </div>
+      <div class="mb-2 col-sm-10">
+        <label for="password" className="col-sm-2 col-form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          className="form-control"
+          id="password"
+          required
+        />
+      </div>
+      <div class="mb-2 col-sm-10">
+        <label for="repeat-password" className="col-sm-2 col-form-label">
+          Repeat Password
+        </label>
+        <input
+          type="repeat-password"
+          name="repeat-password"
+          className="form-control"
+          id="repeat-password"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-outline-primary d-block w-10 m-2"
+      >
+        Next
+      </button>
+      <button
+        type="submit"
+        className="btn btn-outline-secondary d-block w-10 m-2"
+      >
+        Previous
+      </button>
     </form>
   );
 };
